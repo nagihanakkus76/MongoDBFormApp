fedf0e3541b36692131f72a4c7e0afc1435b4951
nagihan <nagihan.akkus.76@gmail.com>
2024-12-25 14:00:43 +0300

    Add project files.

---
+++ MongoDBFormApp/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDBFormApp.Entities
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CustomerId { get; set; }
+        public string CustomerName{ get; set; }
+        public string CustomerSurname{ get; set; }
+        public string CustomerCity{ get; set; }
+        public decimal CustomerBalance{ get; set; }
+        public int CustomerShoppingCount{ get; set; }
+    }
+}
